656538d65daa40a6afdea23ffccb4012d1cc3fbd
Saniaa2715 <135851408+Saniaa2715@users.noreply.github.com>
2025-06-24 11:13:06 +0530

    Create varibles.js

---
+++ varibles.js
@@ -0,0 +1,28 @@
+// WORST WAY!
+x = 5;
+
+// var => AVOID using var
+// declaration + assignment
+var number = 10;
+
+// undefined
+var primeNumber;
+
+primeNumber = 7;
+
+// let => PREFERRED
+let distance = 10;
+distance = 15;
+
+let quantity;
+
+quantity = 25;
+
+// const => PREFERRED OVER LET.
+const PI = 3.14;
+// const name = input()
+// const distanceToSun; // DOES NOT WORK
+
+
+let firstName = "something";
+firstName = 5;
